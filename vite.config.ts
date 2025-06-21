8cb49951bb4d2879b4a44c866d67bc4c40e68742
darviny <darvin@gmail.com>
2025-06-20 23:11:26 -0700

    refactor: modernize Sass syntax and fix deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/colors.scss";`
+        additionalData: `@use "./src/styles/colors.scss" as *;`
       }
     }
   }
